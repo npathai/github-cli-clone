8e7ea3a032c2fdf4e8cc29f1734a484bcaea36cb
Narendra Pathai <narendra.pathai@gmail.com>
2020-10-11 23:33:39 -0400

    Made progress towards making HTTP connection with GitHub

--- github/client.go
+++ github/client.go
@@ -1,6 +1,15 @@
 package github
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
 
 type User struct {
 	Login string `json:"login"`
@@ -91,4 +100,96 @@ func newClientWithHost(host *Host) *Client {
 type Client struct {
 	Host *Host
 	cachedClient *simpleClient
+}
+
+func (client *Client) ensureAccessToken() error {
+	if client.Host.AccessToken == "" {
+		host, err := CurrentConfig().PromptHost(client.Host.Host)
+		if err != nil {
+			return err
+		}
+		client.Host = host
+	}
+	return nil
+}
+
+func (client *Client) apiClient() (c *simpleClient, err error) {
+	unixSocket := os.ExpandEnv(client.Host.UnixSocket)
+	httpClient := newHttpClient(os.Getenv("HUB_TEST_HOST"), os.Getenv("HUB_VERBOSE") != "", unixSocket)
+	apiRoot := client.absolute(normalizeHost(client.Host.Host))
+	if !strings.HasPrefix(apiRoot.Host, "api.github.") {
+		apiRoot.Path = "/api/v3/"
+	}
+
+	return &simpleClient{
+		httpClient: httpClient,
+		rootUrl: apiRoot,
+	}
+}
+
+func (client *Client) simpleApi() (c *simpleClient, err error) {
+	err = client.ensureAccessToken()
+	if err != nil {
+		return
+	}
+
+	if client.cachedClient != nil {
+		c = client.cachedClient
+		return
+	}
+
+	c = client.apiClient()
+	c.PrepareRequest = func(req *http.Request) {
+		clientDomain := normalizeHost(client.Host.Host)
+		if strings.HasPrefix(clientDomain, "api.github.") {
+			clientDomain = strings.TrimPrefix(clientDomain, "api.")
+		}
+		requestHost := strings.ToLower(req.URL.Host)
+		if requestHost == clientDomain || strings.HasSuffix(requestHost, "."+clientDomain) {
+			req.Header.Set("Authorization", "token "+client.Host.AccessToken)
+		}
+	}
+
+	client.cachedClient = c
+	return
+}
+
+func (client *Client) FetchPullRequests(project *Project, filterParams map[string]interface{}, limit int,
+filter func(pr *PullRequest) bool) (prs []PullRequest, err error) {
+
+	api, err := client.simpleApi()
+	if err != nil {
+		return
+	}
+	path := fmt.Sprintf("repos/%s/%s/pulls?per_page=%d", project.Owner, project.Name, perPage(limit, 100))
+	if filterParams != nil {
+		path = addQuery(path, filterParams)
+	}
+
+	prs = []PullRequest{}
+	var res *simpleResponse
+
+	for path != "" {
+		res, err :=	api.GetFile(path, draftsType)
+		if err = checkStatus(200, "fetching pull requests", res, err); err != nil {
+			return
+		}
+		path = res.Link("next")
+
+		pullsPage := []PullRequest{}
+		if err = res.Unmarshal(&pullsPage); err != nil {
+			return
+		}
+
+		for _, pr := range pullsPage {
+			if filter == nil || filter(&pr) {
+				prs = append(prs, pr)
+				if limit > 0 && len(prs) == limit {
+					path = ""
+					break
+				}
+			}
+		}
+	}
+	return
 }
--- github/http.go
+++ github/http.go
@@ -1,8 +1,15 @@
 package github
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"net"
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
+	"time"
 )
 
 type simpleClient struct {
@@ -10,4 +17,80 @@ type simpleClient struct {
 	rootUrl        *url.URL
 	PrepareRequest func(*http.Request)
 	CacheTTL       int
+}
+
+type verboseTransport struct {
+	Transport *http.Transport
+	Verbose bool
+	OverrideURL *url.URL
+	Out io.Writer
+	Colorized bool
+}
+
+// An implementation of http.ProxyFromEnvironment that isn't broken
+func proxyFromEnvironment(req *http.Request) (*url.URL, error) {
+	proxy := os.Getenv("http_proxy")
+	if proxy == "" {
+		proxy = os.Getenv("HTTP_PROXY")
+	}
+	if proxy == "" {
+		return nil, nil
+	}
+
+	proxyURL, err := url.Parse(proxy)
+	if err != nil || !strings.HasPrefix(proxyURL.Scheme, "http") {
+		if proxyURL, err := url.Parse("http://" + proxy); err == nil {
+			return proxyURL, nil
+		}
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("invalid proxy address %q: %v", proxy, err)
+	}
+
+	return proxyURL, nil
+}
+
+func newHttpClient(testHost string, verbose bool, unixSocket string) *http.Client {
+	var testURL *url.URL
+	if testHost != "" {
+		testURL, _ = url.Parse(testHost)
+	}
+	var httpTransport *http.Transport
+	if unixSocket != "" {
+		dialFunc := func(network, addr string) (net.Conn, error) {
+			return net.Dial("unix", unixSocket)
+		}
+		dialContext := func(_ context.Context, _, _ string) (net.Conn, error) {
+			return net.Dial("unix", unixSocket)
+		}
+		httpTransport = &http.Transport{
+			DialContext:           dialContext,
+			DialTLS:               dialFunc,
+			ResponseHeaderTimeout: 30 * time.Second,
+			ExpectContinueTimeout: 10 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+		}
+	} else {
+		httpTransport = &http.Transport{
+			Proxy: proxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			TLSHandshakeTimeout: 10 * time.Second,
+		}
+	}
+	tr := &verboseTransport{
+		Transport:   httpTransport,
+		Verbose:     verbose,
+		OverrideURL: testURL,
+		// FIXME add these once we incorporate ui package
+		Out:         nil,
+		Colorized:   nil,
+	}
+
+	return &http.Client{
+		Transport: tr,
+	}
 }
